77aa000ad4ae36911544fc29fc9e784c32a329c8
Asiful Alam Fahim <asiful.alam@northsouth.edu>
2020-07-17 20:18:43 +0600

    Emoji Converter
    This program will convert your text into emoji.

--- Python/random.py
+++ Python/random.py
@@ -1,1 +1,6 @@
-print("Hello World")
+class fahim:
+    plays = "each of them play  cricket"
+    print(plays)
+
+class rohet(fahim):
+    pass
---
+++ Python/util function access/convert.py
@@ -0,0 +1,6 @@
+from util import find_max
+
+
+numbers=[10,40,20,15,2]
+max=find_max(numbers)
+print(max)
